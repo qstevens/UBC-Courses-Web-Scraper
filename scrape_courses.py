035c93ac2f70d0414cb715f544eff0b41093710c
stevens <stevensqiu@gmail.com>
2020-03-13 14:57:35 -0700

    add aiohttp and asyncio

--- scrape_courses.py
+++ scrape_courses.py
@@ -7,7 +7,9 @@ from bs4 import BeautifulSoup
 from subject import Subject, get_subjects_from_soup
 from course import Course, get_courses_from_subjects_soup, get_courses_description_and_credits_from_soup
 from section import Section, get_sections_from_soup, get_section_info_from_soup
-    
+import asyncio
+from aiohttp import ClientSession
+import time
 
 def format_url(session = "", subject = "", course = "", section = "", pname = "subjarea", tname = ""):
     root = "https://courses.students.ubc.ca/cs/courseschedule?"
@@ -49,7 +51,6 @@ def get_url_soup(url):
     return soup
 
 def get_available_sessions(soup):
-
     session_dropdown_menu = soup.select('.breadcrumb .btn-group')[1]
 
     available_sessions = list(map(lambda x: x.get(
@@ -77,86 +78,151 @@ def prompt_session_selection(available_sessions):
     session = available_sessions[sid]
     return session
 
-def main():
+async def fetch(url, session):
+    async with session.get(url) as response:
+        assert response.status == 200
+        return await response.read()
 
-    subjects_map = {}
-    courses_map = {}
-    sections_map = {}
+# async def run(urls):
+#     tasks = []
 
-    # Select Session Year and Term
-    ubc_courses_url = format_url()
-    course_schedule_soup = get_url_soup(ubc_courses_url)
-    available_sessions = get_available_sessions(course_schedule_soup)
-    session = prompt_session_selection(available_sessions)
+#     # Fetch all responses within one Client session,
+#     # keep connection alive for all requests.
+#     async with ClientSession() as session:
+#         for url in urls:
+#             task = asyncio.create_task(fetch(url, session))
+#             tasks.append(task)
 
-    # Get all Subjects for Session
-    browser_session_subjects_url = format_url(session)
-    subjects_soup = get_url_soup(browser_session_subjects_url)
-    subjects = get_subjects_from_soup(subjects_soup)
+#         # you now have all response bodies in this variable
+#         # print(responses)
+#         return await tasks
 
-    # Get all Courses for Subjects - Async
-    for subject in subjects:
-        print(subject.code)
-        subjects_map[format_url(session, subject.code)] = subject
-    
-    rs = (grequests.get(u) for u in subjects_map.keys())
-    requests = grequests.map(rs)
+async def main():
+    async with ClientSession() as c_session:
 
-    for response in requests:
-        # print(response.url)
-        subject = subjects_map[response.url]
-        subject_courses_soup = BeautifulSoup(response.content, 'lxml')
-        subject.courses = get_courses_from_subjects_soup(subject_courses_soup)
+        start_time = time.time()
 
-        # Save Course hrefs for Async Requests
-        for course in subject.courses:
-            print(course.course_name)
-            courses_map[format_url(session, course.subject_code, course.course_number)] = course
+        subjects_map = {}
+        courses_map = {}
+        sections_map = {}
 
-    # Get all Sections for Courses - Async
-    rs = (grequests.get(u) for u in courses_map.keys())
-    
-    requests = grequests.map(rs)
+        # Select Session Year and Term
+        ubc_courses_url = format_url()
+        course_schedule_soup = get_url_soup(ubc_courses_url)
+        available_sessions = get_available_sessions(course_schedule_soup)
+        session = prompt_session_selection(available_sessions)
 
-    for response in requests:
-        print(response.url)
-        course = courses_map[response.url]
-        courses_soup = BeautifulSoup(response.content, "lxml")
-        course.sections = get_sections_from_soup(courses_soup)
+        # Get all Subjects for Session
+        browser_session_subjects_url = format_url(session)
+        subjects_soup = get_url_soup(browser_session_subjects_url)
+        subjects = get_subjects_from_soup(subjects_soup)
 
-        # Set Course Description and Credits for Course
-        course.description, course.course_credits = get_courses_description_and_credits_from_soup(courses_soup)
+        end_subjects_time = time.time()
+        # Get all Courses for Subjects - Async
+        for subject in subjects:
+            # print(subject.code)
+            subjects_map[format_url(session, subject.code)] = subject
+        print("Subjects:", len(subjects_map.keys()))
         
-        # Save Section hrefs for Async Requests
-        for section in course.sections:
-            sections_map[format_url(session, section.subject_code, section.course_number, section.section_number)] = section
-            print(section.section)
+        # rs = (grequests.get(u) for u in subjects_map.keys())
+        # requests = grequests.map(rs)
 
-    # Get Section Info for Sections - Async
-    rs = (grequests.get(u) for u in sections_map.keys())
-    requests = grequests.map(rs)
+        tasks = []
 
-    for response in requests:
-        section = sections_map[response.url]
-        soup = BeautifulSoup(response.content, "lxml")
-        section_info = get_section_info_from_soup(soup)
+        for url in subjects_map.keys():
+            task = asyncio.create_task(fetch(url, c_session))
+            tasks.append(task)
 
-        # Set Section Info for Section
-        section.building = section_info.building
-        section.room = section_info.room
-        section.instructors = section_info.instructors
-        section.totalRemaining = section_info.totalRemaining
-        section.currentlyRegistered = section_info.currentlyRegistered
-        section.generalRemaining = section_info.generalRemaining
-        section.restrictedRemaining = section_info.restrictedRemaining
+        # you now have all response bodies in this variable
 
-        print(section.instructors)
+        requests = await asyncio.gather(*tasks)
 
-    print(len("Subjects:", subjects_map.keys()))
-    print(len("Courses:", subjects_map.keys()))
-    print(len("Sections:", subjects_map.keys()))
+        for i in range(len(requests)):
+            # print(response.url)
+            subject = subjects_map[list(subjects_map.keys())[i]]
+            subject_courses_soup = BeautifulSoup(requests[i], 'lxml')
+            subject.courses = get_courses_from_subjects_soup(subject_courses_soup)
 
-    return
+            # Save Course hrefs for Async Requests
+            for course in subject.courses:
+                # print(course.course_name)
+                courses_map[format_url(session, course.subject_code, course.course_number)] = course
+        print("Courses:", len(courses_map.keys()))
+
+        end_courses_time = time.time()
+
+        # Get all Sections for Courses - Async
+        # rs = (grequests.get(u) for u in courses_map.keys())
+        # requests = grequests.map(rs)
+                
+        tasks = []
+
+        for url in courses_map.keys():
+            task = asyncio.create_task(fetch(url, c_session))
+            tasks.append(task)
+
+        # you now have all response bodies in this variable
+        
+        requests = await asyncio.gather(*tasks)
+
+        for i in range(len(requests)):
+        # for response in requests:
+            # print(response.url)
+            course = courses_map[list(courses_map.keys())[i]]
+            courses_soup = BeautifulSoup(requests[i], "lxml")
+            course.sections = get_sections_from_soup(courses_soup)
+
+            # Set Course Description and Credits for Course
+            course.description, course.course_credits = get_courses_description_and_credits_from_soup(courses_soup)
+            
+            # Save Section hrefs for Async Requests
+            for section in course.sections:
+                sections_map[format_url(session, section.subject_code, section.course_number, section.section_number)] = section
+                # print(section.section)
+        print("Sections:", len(sections_map.keys()))
+
+        end_sections_time = time.time()
+
+        # Get Section Info for Sections - Async
+        tasks = []
+
+        for url in sections_map.keys():
+            task = asyncio.create_task(fetch(url, c_session))
+            tasks.append(task)
+
+        # you now have all response bodies in this variable
+        
+        requests = await asyncio.gather(*tasks)
+
+        for i in range(len(requests)):
+        # for response in requests:
+            section = sections_map[list(sections_map.keys())[i]]
+            soup = BeautifulSoup(requests[i], "lxml")
+            section_info = get_section_info_from_soup(soup)
+
+            # Set Section Info for Section
+            section.building = section_info.building
+            section.room = section_info.room
+            section.instructors = section_info.instructors
+            section.totalRemaining = section_info.totalRemaining
+            section.currentlyRegistered = section_info.currentlyRegistered
+            section.generalRemaining = section_info.generalRemaining
+            section.restrictedRemaining = section_info.restrictedRemaining
+
+            # print(section.section)
+
+        elapsed_courses_time = end_subjects_time - start_time
+        elapsed_sections_time = end_sections_time - end_courses_time
+        elapsed_section_info_time = time.time() - end_sections_time
+        elapsed_time = time.time() - start_time
+
+        print(session)
+
+
+        print("Time elapsed Courses:", elapsed_courses_time)
+        print("Time elapsed Sections:", elapsed_sections_time)
+        print("Time elapsed Section Info:", elapsed_section_info_time)
+        print("Time elapsed:", elapsed_time)
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
